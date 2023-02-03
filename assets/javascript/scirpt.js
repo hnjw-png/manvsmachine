bbcc73c5d97cdf6fa7e5d17153c4eeb1895c9782
Holly Williams <hollyyeh12@gmail.com>
2023-02-03 22:55:52 +0000

    basic edits added to javascript,

---
+++ assets/javascript/scirpt.js
@@ -0,0 +1,17 @@
+//start of code, players button choice.
+
+//player presses rock, paper or scissors button//
+
+for (let button of buttons) {
+    button.addEventListener(onclick, function);
+    if {
+        document.getMyElementById("rock", onclick)
+    }
+    if {
+        button.addEventistenerById("paper",onclick)
+
+    }
+    if {
+        button.addEventistenerById("scissors", onclick)
+    }
+}
