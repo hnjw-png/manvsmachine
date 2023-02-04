ef890ce15af82fd019413b2a0d621ce841d4669b
Holly Williams <hollyyeh12@gmail.com>
2023-02-04 21:51:00 +0000

    add elements to jaascript, as well as event listener for buttons and choice functions for player and computer.

--- assets/javascript/scirpt.js
+++ assets/javascript/scirpt.js
@@ -1,17 +1,48 @@
-//start of code, players button choice.
+//start of code//
 
-//player presses rock, paper or scissors button//
+//start restart function.//
 
-for (let button of buttons) {
-    button.addEventListener(onclick, function);
-    if {
-        document.getMyElementById("rock", onclick)
-    }
-    if {
-        button.addEventistenerById("paper",onclick)
+//declare elements//
 
-    }
-    if {
-        button.addEventistenerById("scissors", onclick)
-    }
+const buttons = document.getMyElementByClassName("buttons");
+const playerScore = document.getMyElementById("man-score");
+const computerScore = document.getMyElementByiD("computer-score");
+const playerImage = document.getMyElementId("man-image");
+const computerImage = document.getMyElement("computer-image");
+const choice = ["Rock", "Paper", "Scissors"];
+
+//player presses rock, paper or scissors button.//
+for (let buttons of buttons) {
+    button.addEventListener("click", function() {
+    let playerChoice = this.getAttribrute('data-choice');
+    playGame(playerChoice);
+})
 }
+
+//play the game
+function playGame {
+    playerImage.scr = 'assets/images/${choices[player-choice]}.jpeg;
+
+
+    let computerChoice = Math.random();
+    if (computerChoice <= 0.34){
+        computerChoice = "Rock";
+    } else if (computerChoice <= 0.68){
+        computerChoice = "Paper";
+    } else if (computerChoice >= 0.68) {
+        computerChoice = "Scissors" ;
+    }
+    
+    computerImage.src = 'assets/images/${choices[player-choice]}.jpeg;
+}
+
+
+
+
+//calculate scores according to which of the rock, paper and scissors competes and wins against each other.//
+
+
+//calculate score best of three rounds
+
+
+
